2c8407c4f6c3f05d4b032c73da4830a379b9a933
Séverine Guillemin <sguillemin.dev@gmail.com>
2020-03-23 07:49:07 +0100

    Create admin_template.php

---
+++ view/admin/admin_template.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jean Forteroche</title>
+    <link rel="stylesheet" href="<?php echo ASSETS;?>style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Sacramento&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/968601638e.js" crossorigin="anonymous"></script>
+
+
+    <!-- CDN TinyMCE -->
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>tinymce.init({selector: '.mytextarea'});</script>
+</head>
+
+<body>
+
+    <!-- Header -->
+    <header>
+        <div id="photoJF">
+            <img src="<?php echo ASSETS?>img/portrait.jpg" alt="Portrait de Jean Forteroche";/>
+        </div>
+
+        <h1>Billet simple pour l' Alaska</h1>
+
+        <div>
+            <i class="fas fa-user-circle"></i>
+            <?php echo $_SESSION['username']; ?>
+        </div>
+
+    </header>
+
+
+    <!-- Main -->
+    <main>
+        <?php echo $main;?>
+    </main>
+
+    <!-- Footer -->
+    <footer>
+        <div id="mentions_legales">Mentions légales</div>
+        
+        <div id="forms">
+            <!-- Connexion form -->
+            
+            <form action="<?php echo HOST;?>deconnexion.html" method="post">
+                <input type="submit" value="Se déconnecter" />
+            </form>
+        </div>
+
+    </footer>
+
+
+</body>
+</html>
