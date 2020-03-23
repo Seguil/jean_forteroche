e0d0a11d63d0028eb09680f5312665b206bb2fcd
Séverine Guillemin <sguillemin.dev@gmail.com>
2020-03-23 15:25:30 +0100

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const navChapters1 = new NavChapters(
+    'nav_chapters',
+    '<?php echo CLASSES;?>Routeur.php'
+);
+navChapters1.displayChapters();
+
+// window.addEventListener('load', () => { //fonction fléchée sur 2 lignes = accolades/ fonction fléchée sur 1 ligne = pas d'accolades
+//     navChapters1.displayChapters() //chargement de la map
+// });
