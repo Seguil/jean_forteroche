71d423cc584ce78066c6aee35d7c8880c27823d0
Seguil <60090731+Seguil@users.noreply.github.com>
2020-04-06 18:57:42 +0200

    Update admin_home_page.php
    icones admin CRUD

--- view/admin/admin_home_page.php
+++ view/admin/admin_home_page.php
@@ -45,13 +45,54 @@
                     <input type="text" name="title" id="title" />
                 <label for="content">Contenu</label>
                     <input type="text" name="content" class="mytextarea" />
-                <input type="submit" value="Publier" />
+                <!-- Enregistrer en tant que brouillon -->
+                <button type="submit" value="Enregistré"><i class="fas fa-save"></i></button>
+                <!-- Publier -->
+                <button type="submit" value="Publié"><i class="fas fa-file-export"></i></button>
             </form>
+
+            <div id="admin_buttons">
+                <a href="<?php echo HOST;?>read-billet-saved.html" title="Chapitres non publiés" class="tooltip"><i class="fas fa-plus-square"></i></a>
+                <a href="<?php echo HOST;?>delete-billet.html" title="Supprimer" class="tooltip"><i class="fas fa-trash-alt"></i></a>
+                <a href="<?php echo HOST;?>update-billet.html" title="Modifier" class="tooltip"><i class="fas fa-pencil-alt"></i></a>
+            </div>
+
+            <!-- Affiche les articles enregistrés et non publié -->
+            <div id="non_published_billets">
+                <h3>Chapitres non publiés</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Chapitre n°</td>
+                            <td>Titre</td>
+                            <!-- <td>Pseudo</td> -->
+                            <td>Date d'enregistrement</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if(isset($billet)) {
+                            foreach($billet as $bil):?>
+                                <tr>
+                                    <td><?php echo $bil->getId(); ?></td>
+                                    <td><?php echo $bil->getTitle(); ?></td>
+                                    <td><?php echo $com->getPublicationDate()->format('d/m/Y'); ?></td>
+                                    <td><a href="<?php echo HOST;?>delete-non-published-billet.html" title="Supprimer" class="tooltip"><i class="fas fa-trash-alt"></i></a></td>
+                                    <td><a href="<?php echo HOST;?>update-non-published-billet" title="Modifier" class="tooltip"><i class="fas fa-pencil-alt"></i></a></td>
+                                    <td><a href="<?php echo HOST;?>read-non-published-billet.html" title="Lire plus" class="tooltip"><i class="fas fa-plus-square"></i></a></td>
+                                </tr>
+                            <?php endforeach ;
+                        };?>
+                    </tbody>
+                </table>
+            </div>
         </div>
+
+        
 
         <!-- Récupère les commentaires signalés-->
         <div id="report_comments">
             <div class="comment">
+                <h3>Commentaires signalés</h3>
                 <table>
                     <thead>
                         <tr>
@@ -71,7 +112,6 @@
                                     <td><a href="<?php echo HOST;?>delete-report-comment.html" title="Supprimer" class="tooltip"><i class="fas fa-trash-alt"></i></a></td>
                                     <td><a href="<?php echo HOST;?>update-report-comment.html" title="Modifier" class="tooltip"><i class="fas fa-pencil-alt"></i></a></td>
                                     <td><a href="<?php echo HOST;?>answer-report-comment.html" title="Répondre" class="tooltip"><i class="fas fa-edit"></i></a></td>
-                                    <td><a href="<?php echo HOST;?>read-report-comment.html" title="Lire plus" class="tooltip"><i class="fas fa-plus-square"></i></a></td>
                                 </tr>
                             <?php endforeach ;
                         };?>
@@ -80,11 +120,39 @@
             </div>
         </div>
 
-        <div id="admin_buttons">
-            <button>Modifier</button>
-            <button>Supprimer</button>
-            <button>Publier</button>
+
+        <!-- Récupère les commentaires signalés-->
+        <div id="last_comments">
+            <div class="last_comments">
+                <h3>Derniers commentaires</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Chapitre n°</td>
+                            <td>Commentaire</td>
+                            <!-- <td>Pseudo</td> -->
+                            <td>Date</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if(isset($comment)) {
+                            foreach($comment as $com):?>
+                                <tr>
+                                    <td><?php echo $com->getIdBillet(); ?></td>
+                                    <td><?php echo $com->getComment(); ?></td>
+                                    <td><?php echo $com->getCommentDate()->format('d/m/Y'); ?></td>
+                                    <td><a href="<?php echo HOST;?>delete-report-comment.html" title="Supprimer" class="tooltip"><i class="fas fa-trash-alt"></i></a></td>
+                                    <td><a href="<?php echo HOST;?>update-report-comment.html" title="Modifier" class="tooltip"><i class="fas fa-pencil-alt"></i></a></td>
+                                    <td><a href="<?php echo HOST;?>answer-report-comment.html" title="Répondre" class="tooltip"><i class="fas fa-edit"></i></a></td>
+                                </tr>
+                            <?php endforeach ;
+                        };?>
+                    </tbody>
+                </table>
+            </div>
         </div>
+
+
     </article>
 
 </div>
