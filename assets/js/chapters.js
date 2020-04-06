53daf18257a3282951146adb32798497c20a2962
Seguil <60090731+Seguil@users.noreply.github.com>
2020-04-06 08:52:38 +0200

    Update chapters.js

--- assets/js/chapters.js
+++ assets/js/chapters.js
@@ -6,23 +6,24 @@ class Chapters {
 
     displayChapters() {
         const requestBillets = new Ajax () //Je crée une requête des stations qui est une nouvelle requête Ajax. Elle prendra comme paramètres l'url et la réponse
-        
+        console.log(requestBillets);
+       
         requestBillets.ajaxGet(this.url, (response) => {
             let dataBillets = JSON.parse(response);
-            // console.log(response);
+            console.log(dataBillets);
             for (let i=0; i<dataBillets.length; i++) {
                 let billet = dataBillets[i]; //Je récupère les stations sous forme de tableau
                 // Je récupère les informations pour l'affichage des infos 
                 let numberBillet = document.createElement("h2");
-                numberBillet.innerHTML = billet.number;
+                numberBillet.textContent = billet.number;
                 this.id.appendChild(numberBillet);
         
                 let titleBillet = document.createElement("h2");
-                titleBillet.innerHTML = billet.title;
+                titleBillet.textContent = billet.title;
                 this.id.appendChild(titleBillet);
         
                 let contentBillet = document.createElement("p");
-                contentBillet.innerHTML = billet.content;
+                contentBillet.textContent = billet.content;
                 this.id.appendChild(contentBillet);
             };            
         });
